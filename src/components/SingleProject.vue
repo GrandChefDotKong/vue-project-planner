454ff0433654596ede3a8905294f4f6a6f5eb704
Adrien Pauchet <grandchefdotkong@gmail.com>
2022-05-07 10:21:22 +0900

    first commit

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="project">
+      <div class="actions">
+          <h3>{{ project.title }}</h3>
+      </div>
+      <div class="details">
+          {{ project.details }}
+      </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+ data() {
+
+ },
+ props: ['project'],
+}
+</script>
+
+<style scoped>
+  .project {
+    margin: 20px auto;
+    background: white;
+    padding: 10px 20px;
+    border-radius: 4px;
+    box-shadow: 1px 2px 3px rgba(0,0,0,0.05);
+    border-left: 4px solid #e90074;
+  }
+  h3 {
+    cursor: pointer;
+  }
+</style>
